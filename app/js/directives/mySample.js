507fb6888a097fb61400825e384a224b46eb01fa
Bryan Astacio <brya0196@gmail.com>
2018-08-23 10:03:57 -0400

    isolating the scopes of the custom directives

--- app/js/directives/mySample.js
+++ app/js/directives/mySample.js
@@ -3,6 +3,9 @@
 eventsApp.directive('mySample', function($compile) {
   return {
     restrict: 'C',
-    template: "<input type='text' ng-model='sampleData'/> {{sampleData}} <br/>"
+    template: "<input type='text' ng-model='sampleData'/> {{sampleData}} <br/>",
+    scope: {
+      
+    }
   }
 });
